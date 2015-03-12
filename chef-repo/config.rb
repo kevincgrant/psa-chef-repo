3e3241bb64d9b1b54651d9e710fe142c86172b12
Kevin Grant <kevin.grant@catalinamarketing.com>
2015-03-12 00:43:16 +0000

    updated config

--- chef-repo/config.rb
+++ chef-repo/config.rb
@@ -1,2 +1,4 @@
 file_cache_path "/root/chef-cache"
 cookbook_path "/root/chef-repo/cookbooks"
+data_bag_path "/root/chef-repo/data_bags"
+encrypted_data_bag_secret "/root/chef-repo/.chef/data_bag_key"
